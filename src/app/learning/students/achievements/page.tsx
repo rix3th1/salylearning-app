a368b08ca8a3acbd751b6075d46cf965983e3e59
Richard A. Rich <rojasricor@gmail.com>
2024-05-08 21:56:35 -0500

    adding student pages but missing styles

--- src/app/learning/students/achievements/page.tsx
+++ src/app/learning/students/achievements/page.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import Image from "next/image";
+import genioDelQuizLogro from "@/assets/geniodelquiz.png";
+import genioEmergenteLogro from "@/assets/genioemergente.png";
+import faroDelSaberLogro from "@/assets/farodelsaber.png";
 
 export const metadata: Metadata = {
   title: "Logros: Estudiantes | Saly Learning",
@@ -14,63 +18,80 @@ export default function AchievementsPage() {
           </h1>
         </div>
       </div>
-      <section className="contenedor-logros">
-        <div className="fila_principal">
-          <div className="columna_principal">
-            <div className="subcolumna">
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <td className="table-logros">
-                      <h4>Libros Leídos</h4>
-                      <p id="logros-libros">2</p>
-                    </td>
-                    <td className="table-logros">
-                      <h4>Preguntas Correctas</h4>
-                      <p id="logros-preguntas">2</p>
-                    </td>
-                    <td className="table-logros">
-                      <h4>Tiempo de Lectura (min)</h4>
-                      <p id="logros-tiempo">2</p>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="subcolumna">
-              <h4>Medallas Ganadas</h4>
-              <div className="row">
-                <div className="col-md-3">
-                  <img
-                    src="/img/genioemergente.png"
-                    alt="Medalla 1"
-                    className="img-logros"
-                  />
-                  <p>Leer 10 libros</p>
-                </div>
-                <div className="col-md-3">
-                  <img
-                    src="/img/geniodelquiz.png"
-                    alt="Medalla 2"
-                    className="img-logros"
-                  />
-                  <p>Responder 100 preguntas correctamente</p>
-                </div>
-                <div className="col-md-3">
-                  <img
-                    src="/img/farodelsaber.png"
-                    alt="Medalla 2"
-                    className="img-logros"
-                  />
-                  <p>Responder 100 preguntas correctamente</p>
+
+      <section className="container-fluid">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-8">
+              <div className="subcolumna">
+                <div className="table-responsive">
+                  <table className="tabla">
+                    <tbody>
+                      <tr>
+                        <td className="tabla-logros">
+                          <h4>Libros Leídos</h4>
+                          <p id="logros-libros">2</p>
+                        </td>
+                        <td className="tabla-logros">
+                          <h4>Preguntas Correctas</h4>
+                          <p id="logros-preguntas">2</p>
+                        </td>
+                        <td className="tabla-logros">
+                          <h4>Tiempo de Lectura (min)</h4>
+                          <p id="logros-tiempo">2</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
               </div>
-            </div>
-            <div className="subcolumna">
-              <div className="row">
+
+              <div className="subcolumna">
+                <div className="col-md-12">
+                  <h4 style={{ marginBottom: "2rem" }}>Medallas Ganadas</h4>
+                  <div className="col-md-4">
+                    <Image
+                      src={genioEmergenteLogro}
+                      alt="Medalla 1"
+                      className="img-logros"
+                      width={200}
+                      height={300}
+                      quality={100}
+                      placeholder="blur"
+                    />
+                    <p>Leer 10 libros</p>
+                  </div>
+                  <div className="col-md-4">
+                    <Image
+                      src={genioDelQuizLogro}
+                      alt="Medalla 2"
+                      className="img-logros"
+                      width={200}
+                      height={300}
+                      quality={100}
+                      placeholder="blur"
+                    />
+                    <p>Responder 100 preguntas correctamente</p>
+                  </div>
+                  <div className="col-md-4">
+                    <Image
+                      src={faroDelSaberLogro}
+                      alt="Medalla 2"
+                      className="img-logros"
+                      width={200}
+                      height={300}
+                      quality={100}
+                      placeholder="blur"
+                    />
+                    <p>Responder 100 preguntas correctamente</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="subcolumna">
                 <div className="col-md-6">
                   <h4>Historial de Logros</h4>
-                  <ul className="list-group">
+                  <ul className="list-group" style={{ color: "#317DFF" }}>
                     <li className="list-group-item">
                       Leídos 5 libros (02/03/2024)
                     </li>
@@ -91,15 +112,23 @@ export default function AchievementsPage() {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="columna_secundaria">
-            <div className="col-chart">
-              <h4>Progreso en Lectura</h4>
-              <canvas id="logros-chart-1" width={200} height={200} />
-            </div>
-            <div className="col-chart">
-              <h4>Preguntas Respondidas Correctamente</h4>
-              <canvas id="logros-chart-2" width={200} height={200} />
+
+            <div
+              className="col-md-4"
+              style={{
+                backgroundColor: "#97CFF9",
+                borderRadius: "10px",
+                padding: "2rem 0",
+              }}
+            >
+              <div className="col-chart">
+                <h4>Progreso en Lectura</h4>
+                <canvas id="logros-chart-1" width={200} height={200} />
+              </div>
+              <div className="col-chart">
+                <h4>Preguntas Respondidas Correctamente</h4>
+                <canvas id="logros-chart-2" width={200} height={200} />
+              </div>
             </div>
           </div>
         </div>
