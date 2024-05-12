8bcdd85fc03a12132d9a65cf438e2fc66d8b438e
Richard A. Rich <rojasricor@gmail.com>
2024-05-11 22:11:28 -0500

    feat: Add PowerButton component for system logout functionality

---
+++ src/components/ui/PowerButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { MdOutlinePowerSettingsNew } from "react-icons/md";
+import Swal from "sweetalert2";
+
+const showSignOutSwal = () => {
+  Swal.fire({
+    title: "¿Estás seguro de que quieres salir?",
+    showCancelButton: true,
+    confirmButtonText: "Sí, salir",
+    cancelButtonText: "Cancelar",
+    icon: "question",
+  }).then((result) => result.isConfirmed && signOut());
+};
+
+export default function PowerButton() {
+  return (
+    <li
+      className="tooltips-general exit-system-button"
+      title="Salir del sistema"
+      onClick={showSignOutSwal}
+    >
+      <MdOutlinePowerSettingsNew style={{ fontSize: 25, marginTop: 15 }} />
+    </li>
+  );
+}
