ce1678241fcfd7f4c119ec34cddcd99d5c852bd9
Richard A. Rich <rojasricor@gmail.com>
2024-05-13 14:52:24 -0500

    refactor: Add contactos service for creating contact form submissions

---
+++ src/services/contactos.service.ts
@@ -0,0 +1,24 @@
+import { api } from "./api.service";
+
+export const contactoInitState = {
+  nombre_completo: "",
+  email: "",
+  telefono: "",
+  mensaje: "",
+};
+
+export async function crearContacto(formData: typeof contactoInitState) {
+  const res = await api("/contactos", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
