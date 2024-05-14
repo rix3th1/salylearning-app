e4c64fd0738594bbcb3a29183d2711a77fe67ef5
Richard A. Rich <rojasricor@gmail.com>
2024-05-13 23:20:03 -0500

    feat: Add password recovery functionality

--- src/services/recuperar-clave.service.ts
+++ src/services/recuperar-clave.service.ts
@@ -1,10 +1,51 @@
 import { api } from "./api.service";
 
+export const cambiarClaveInitState = {
+  password: "",
+  confirmar_password: "",
+};
+
 export async function enviarEmailDeRecuperacion(email: string) {
   const res = await api("/recuperar-clave/enviar-email-de-recuperacion", {
     method: "POST",
     body: JSON.stringify({ email }),
     headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
+
+export async function validarToken(token: string) {
+  const res = await api(`/recuperar-clave/validar-token?token=${token}`, {
+    method: "GET",
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
+
+export async function cambiarClave(
+  formData: typeof cambiarClaveInitState,
+  token: string
+) {
+  const res = await api("/recuperar-clave/cambiar-clave", {
+    method: "PATCH",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 
   const data = await res.json();
