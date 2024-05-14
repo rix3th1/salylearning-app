e243c1995a00635db4420d154685237cd7a3c76d
Richard A. Rich <rojasricor@gmail.com>
2024-05-13 23:19:26 -0500

    feat: Add change password page

---
+++ src/app/(pagina principal)/change-password/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import ChangePswSection from "./components/ChangePswSection";
+
+export const metadata: Metadata = {
+  title: "Software Saly | Cambiar contraseña",
+};
+
+export default function ContactPage() {
+  return (
+    <section className="py-5">
+      <div className="container px-5">
+        <div className="bg-light rounded-4 py-5 px-4 px-md-5">
+          <ChangePswSection />
+        </div>
+      </div>
+    </section>
+  );
+}
