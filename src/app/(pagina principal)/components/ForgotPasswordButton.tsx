99e84c6632d057f2b4ff24e963c99a1a168a18a4
Richard A. Rich <rojasricor@gmail.com>
2024-05-17 17:16:26 -0500

    refactor: Update ForgotPasswordButton component to use Swal for email recovery

--- src/app/(pagina principal)/components/ForgotPasswordButton.tsx
+++ src/app/(pagina principal)/components/ForgotPasswordButton.tsx
@@ -3,45 +3,45 @@ import { enviarEmailDeRecuperacion } from "@/services/recuperar-clave.service";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
-export default function ForgotPasswordButton() {
-  const showSwalForSendEmail = async () => {
-    const result = await withReactContent(Swal).fire({
+const showSwalForSendEmail = async () => {
+  const result = await withReactContent(Swal).fire({
+    customClass: plus_jakarta_sans.className,
+    title: "¿Olvidaste tu contraseña?",
+    text: "Por favor ingresa tu correo electrónico",
+    input: "email",
+    inputPlaceholder: "Escribe aquí tu correo electrónico",
+    showCancelButton: true,
+    confirmButtonText: "Enviar",
+    cancelButtonText: "Cancelar",
+    showLoaderOnConfirm: true,
+    preConfirm: async (email) => {
+      if (!email) {
+        Swal.showValidationMessage("Debes escribir tu correo electrónico");
+      }
+
+      try {
+        return await enviarEmailDeRecuperacion(email);
+      } catch (error) {
+        if (error instanceof Error) {
+          Swal.showValidationMessage(error.message.replace(/,/g, ", "));
+        }
+      }
+    },
+    allowOutsideClick: () => !Swal.isLoading(),
+  });
+
+  if (result.isConfirmed) {
+    Swal.fire({
       customClass: plus_jakarta_sans.className,
-      title: "¿Olvidaste tu contraseña?",
-      text: "Por favor ingresa tu correo electrónico",
-      input: "email",
-      inputPlaceholder: "Escribe aquí tu correo electrónico",
-      showCancelButton: true,
-      confirmButtonText: "Enviar",
-      cancelButtonText: "Cancelar",
-      showLoaderOnConfirm: true,
-      preConfirm: async (email) => {
-        if (!email) {
-          Swal.showValidationMessage("Debes escribir tu correo electrónico");
-        }
-
-        try {
-          return await enviarEmailDeRecuperacion(email);
-        } catch (error) {
-          if (error instanceof Error) {
-            Swal.showValidationMessage(error.message.replace(/,/g, ", "));
-          }
-        }
-      },
-      allowOutsideClick: () => !Swal.isLoading(),
+      title: "¡Correo enviado!",
+      text: result.value.message,
+      icon: "success",
+      timer: 3000,
     });
+  }
+};
 
-    if (result.isConfirmed) {
-      Swal.fire({
-        customClass: plus_jakarta_sans.className,
-        title: "¡Correo enviado!",
-        text: result.value.message,
-        icon: "success",
-        timer: 3000,
-      });
-    }
-  };
-
+export default function ForgotPasswordButton() {
   return (
     <button type="button" className="olvide" onClick={showSwalForSendEmail}>
       Olvidé mi contraseña?
