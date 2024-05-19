1909556d6ea3e6208770e4f8bb632dc836560238
Richard A. Rich <rojasricor@gmail.com>
2024-05-19 15:51:36 -0500

    feat: Add contactoYSoporteInitState to contactos.service.ts

--- src/services/contactos.service.ts
+++ src/services/contactos.service.ts
@@ -7,8 +7,33 @@ export const contactoInitState = {
   mensaje: "",
 };
 
+export const contactoYSoporteInitState = {
+  nombre_completo: "",
+  email: "",
+  asunto: "",
+  mensaje: "",
+};
+
 export async function crearContacto(formData: typeof contactoInitState) {
   const res = await fetchClient("/contactos", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
+
+export async function crearContactoYSoporte(
+  formData: typeof contactoYSoporteInitState
+) {
+  const res = await fetchClient("/soporte", {
     method: "POST",
     body: JSON.stringify(formData),
     headers: { "Content-Type": "application/json" },
