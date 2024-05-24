b69d332b9aa762870bad8bfc75deafe200a1c498
Richard A. Rich <rojasricor@gmail.com>
2024-05-24 14:42:15 -0500

    refactor: Update loading component to display four spans for loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,9 +4,10 @@ export default function LoadingPage() {
   return (
     <div className="loader-container">
       <div className="loader">
-        {Array.from({ length: 4 }, (_, i) => (
-          <span key={i} />
-        ))}
+        <span />
+        <span />
+        <span />
+        <span />
       </div>
     </div>
   );
