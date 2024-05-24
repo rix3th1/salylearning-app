edddb676e0e841498987118762a0866bbb15b701
Richard A. Rich <rojasricor@gmail.com>
2024-05-24 14:23:53 -0500

    refactor: Add usuarios.service.ts for updating user data

---
+++ src/services/usuarios.service.ts
@@ -0,0 +1,32 @@
+import { fetchClient } from "./api.service";
+
+export const actualizarUsuarioInitState = {
+  p_nombre: "",
+  s_nombre: "",
+  p_apellido: "",
+  s_apellido: "",
+  edad: "",
+};
+
+export async function actualizarUsuario(id_usuario: string, formData: any) {
+  const res = await fetchClient(`/usuarios/${id_usuario}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      ...formData,
+      edad: parseInt(formData.edad || "0"),
+      s_nombre: formData.s_nombre || null,
+      s_apellido: formData.s_apellido || null,
+      ciudad: formData.ciudad || null,
+      fecha_nacimiento: formData.fecha_nacimiento || null,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
