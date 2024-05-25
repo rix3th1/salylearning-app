284f4e10af38f772037bb71eb697aaff8831be3b
Richard A. Rich <rojasricor@gmail.com>
2024-05-25 14:26:33 -0500

    refactor: Update ModalAvatar component to use avatar_id instead of selectedAvatar
    The ModalAvatar component has been refactored to use the avatar_id variable instead of the selectedAvatar variable. This change ensures that the correct avatar is selected and updated for the user. The recent user commits and repository commits indicate a focus on improving profile-related features and components.

--- src/app/learning/components/ModalAvatar.tsx
+++ src/app/learning/components/ModalAvatar.tsx
@@ -1,37 +1,73 @@
+import { actualizarAvatarUsuario } from "@/services/avatar-usuario.service";
+import { obtenerAvatars } from "@/services/avatars.service";
 import { User } from "next-auth";
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import "../css/modal-avatar.style.css";
-import avatars from "../meta/avatars.json";
 
 interface IProps {
   avatarModal: React.RefObject<HTMLDivElement>;
   userType: User["rol"];
+  avatar_id: string;
 }
 
-export default function ModalAvatar({ avatarModal, userType }: IProps) {
+export default function ModalAvatar({
+  avatarModal,
+  userType,
+  avatar_id,
+}: IProps) {
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
+  const [idAvatarSelected, setIdAvatarSelected] = useState<number | null>(null);
+  const [avatars, setAvatars] = useState([]);
 
-  const handleAvatarClick = (avatar: string) => {
-    setSelectedAvatar(avatar);
+  const router = useRouter();
+
+  const handleAvatarClick = (id_avatar: number) => {
+    setIdAvatarSelected(id_avatar);
   };
 
   const handleAcceptClick = async () => {
     setIsLoading(true);
 
     try {
-      if (!selectedAvatar) {
+      if (!idAvatarSelected) {
         return toast.error("Por favor, selecciona un avatar.");
       }
 
-      console.log(`Avatar seleccionado: ${selectedAvatar}`);
+      await actualizarAvatarUsuario(avatar_id, {
+        id_avatar: `${idAvatarSelected}`,
+      });
+
+      toast.success("Avatar actualizado correctamente!");
+      router.refresh();
     } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message.replace(/,/g, ", "));
+      }
     } finally {
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    toast.promise(
+      obtenerAvatars(userType)
+        .then((data) => {
+          setAvatars(data);
+        })
+        .catch((error) => {
+          if (error instanceof Error) {
+            toast.error(error.message.replace(/,/g, ", "));
+          }
+        }),
+      {
+        loading: "Cargando avatares...",
+        success: "Listo",
+      }
+    );
+  }, []);
 
   return (
     <div className="avatar-modal" ref={avatarModal}>
@@ -50,18 +86,18 @@ export default function ModalAvatar({ avatarModal, userType }: IProps) {
           Por favor, selecciona tu foto de perfíl:
         </h3>
         <div id="avatar-options">
-          {avatars[userType].map(({ src, alt }, i) => (
+          {avatars.map((avatar: any, i) => (
             <Image
               key={i}
               className={`avatar-option${
-                selectedAvatar === src ? " selected" : ""
+                idAvatarSelected === avatar.id ? " selected" : ""
               }`}
-              src={`/img/avatars/${userType}/${src}`}
-              alt={alt}
+              src={`/img/avatars/${userType}/${avatar.nom_avatar}`}
+              alt={avatar.descripcion}
               width={100}
               height={100}
               quality={75}
-              onClick={() => handleAvatarClick(src)}
+              onClick={() => handleAvatarClick(avatar.id)}
             />
           ))}
         </div>
