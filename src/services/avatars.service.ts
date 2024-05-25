2e8176965c4d3dec5b752afb3730182cfba803a2
Richard A. Rich <rojasricor@gmail.com>
2024-05-25 14:24:42 -0500

    feat: Add obtenerAvatars function to avatars.service.ts
    This commit adds the obtenerAvatars function to the avatars.service.ts file. The function fetches avatars based on the user's role by making a GET request to the server. This change enhances the functionality of the service by providing a way to obtain avatars for user profiles.

---
+++ src/services/avatars.service.ts
@@ -0,0 +1,14 @@
+import { User } from "next-auth";
+import { fetchClient } from "./api.service";
+
+export async function obtenerAvatars(rol: User["rol"]) {
+  const res = await fetchClient(`/avatar/rol?rol=${rol}`, { method: "GET" });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
