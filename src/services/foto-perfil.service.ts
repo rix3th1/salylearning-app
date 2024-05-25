92dc2919f46aed06d2c0c606013b8f6dffad8c78
Richard A. Rich <rojasricor@gmail.com>
2024-05-24 23:52:41 -0500

    refactor: Create foto-perfil.service.ts for handling profile photo uploads

---
+++ src/services/foto-perfil.service.ts
@@ -0,0 +1,16 @@
+import { fetchClient } from "./api.service";
+
+export async function crearFotoPerfil(formData: FormData) {
+  const res = await fetchClient("/foto-perfil", {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
