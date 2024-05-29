29e8b9d1ad9e440bb7bcae7ecd47b756dc29024c
Richard A. Rich <rojasricor@gmail.com>
2024-05-28 22:16:50 -0500

    refactor: Update Nav component to collapse navigation menu on link click
    The Nav component has been updated to include a collapseNav function that collapses the navigation menu when a link is clicked. This change improves the user experience by automatically closing the menu after a selection is made. The recent user commits and repository commits indicate a focus on improving navigation and user interface components.

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function Nav() {
+  const navButton = useRef<HTMLButtonElement>(null);
+  const linksContainerRef = useRef<HTMLDivElement>(null);
+
+  function collapseNav() {
+    if (navButton.current && linksContainerRef.current) {
+      navButton.current.classList.add("collapsed");
+      linksContainerRef.current.classList.remove("show");
+    }
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
       <div className="container px-5">
-        <Link className="navbar-brand" href="/">
+        <Link onClick={collapseNav} className="navbar-brand" href="/">
           <span className="fw-bolder text-primary">SALY LEARNING</span>
         </Link>
         <button
+          ref={navButton}
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -18,25 +32,29 @@ export default function Nav() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          ref={linksContainerRef}
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
             <li className="nav-item">
-              <Link className="nav-link" href="/">
+              <Link onClick={collapseNav} className="nav-link" href="/">
                 Inicio
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" href="/resume">
+              <Link onClick={collapseNav} className="nav-link" href="/resume">
                 ¿Listos para aprender?
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" href="/projects">
+              <Link onClick={collapseNav} className="nav-link" href="/projects">
                 ¿Por qué nosotros?
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" href="/contact">
+              <Link onClick={collapseNav} className="nav-link" href="/contact">
                 Contáctenos
               </Link>
             </li>
