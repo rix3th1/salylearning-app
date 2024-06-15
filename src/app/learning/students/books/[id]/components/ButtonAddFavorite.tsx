9f95e99a738f63c7b33b0d8ae4cf46b859469731
Richard A. Rich <rojasricor@gmail.com>
2024-06-15 13:55:01 -0500

    improved add to favorite books functionality

--- src/app/learning/students/books/[id]/components/ButtonAddFavorite.tsx
+++ src/app/learning/students/books/[id]/components/ButtonAddFavorite.tsx
@@ -2,6 +2,7 @@
 
 import { crearMiLibro, eliminarMiLibro } from "@/services/mis-libros.service";
 import { useRouter } from "next-nprogress-bar";
+import { useState } from "react";
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
 interface IProps {
@@ -11,12 +12,16 @@ interface IProps {
 }
 
 export default function ButtonAddFavorite({ libro, favorito, user }: IProps) {
+  const [isFavorite, setIsFavorite] = useState(favorito);
+
   const router = useRouter();
 
   const handleClickFavorite = async () => {
     if (favorito) {
+      setIsFavorite(false);
       await eliminarMiLibro(favorito.id);
     } else {
+      setIsFavorite(true);
       await crearMiLibro({ id_libro: libro.id, id_usuario: user.id });
     }
 
@@ -29,7 +34,7 @@ export default function ButtonAddFavorite({ libro, favorito, user }: IProps) {
       style={{ backgroundColor: "#dd1e82", color: "#fff" }}
       onClick={handleClickFavorite}
     >
-      {favorito ? (
+      {isFavorite ? (
         <MdFavorite style={{ position: "relative", top: 3 }} />
       ) : (
         <MdFavoriteBorder style={{ position: "relative", top: 3 }} />
