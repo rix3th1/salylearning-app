241466bc37b6601e7ab41820c71b2b6732aceef2
Richard A. Rich <rojasricor@gmail.com>
2024-06-21 21:48:17 -0500

    Improving SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" suppressHydrationWarning>
+    <html lang="es">
       <head>
         <link
           rel="icon"
